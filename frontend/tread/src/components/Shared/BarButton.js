58d3fe1b7005076635e9738d23bbf9d62fee9838
Prabhdeep Kainth <pdkainth@gmail.com>
2023-02-22 17:17:17 -0800

    separate backend

--- frontend/tread/src/components/Shared/BarButton.js
+++
@@ -1,6 +0,0 @@
-import '../../css/Shared/BarButton.css'
-
-const BarButton = (props) => {
-    return (<button onClick = {props.function} className = "BarButton">{props.name}</button>);
-}
-export default BarButton;
