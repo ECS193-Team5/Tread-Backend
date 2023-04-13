04fc26c312bbad17799d13852785315d9e971fb7
PlazmaZero <Howardw117@gmail.com>
2023-04-12 17:43:04 -0700

    trying to use secret manager and debugging

--- firebase_startup.js
+++ firebase_startup.js
@@ -1,7 +1,9 @@
 const firebase = require("firebase-admin");
+require('dotenv').config()
 
 // Best practice: Get the credential file and db url from environment varible
 const accountCred = process.env.ACCOUNT_CREDENTIAL
+console.log("here is path", )
 const serviceAccount = require("./tread-379302-c0d24a7ca7b0.json");
 // not sure if we need
 const dbUrl = "https://<Your DB>.firebaseio.com"; //You’ll get the DB Url from Firebase Console
