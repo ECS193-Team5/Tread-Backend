9f2319da05197d11363696bf200d26150c587df0
PlazmaZero <Howardw117@gmail.com>
2023-04-23 00:04:50 -0700

    refactored medal testing

--- tests/medals.spec.js
+++ tests/medals.spec.js
@@ -42,46 +42,55 @@ describe('Testing Medals System', () => {
         sandbox.restore();
     })
 
-    it("test addMedal on save success", async function () {
-        req = { "body" :{"unit":"m", "amount": 10, "exerciseName":"Basketball", "level":1}};
-        sandbox.stub(mongoose.Model.prototype, 'save').resolves("");
-        let addMedal = medals.__get__("addMedal");
-        await addMedal(req, res);
-        expect(res.status).to.equal(200);
+    describe("Test addMedal", () => {
+        let addMedal;
+        beforeEach(() => {
+            req = { "body" :{"unit":"m", "amount": 10, "exerciseName":"Basketball", "level":1}};
+            saveStub = sandbox.stub(mongoose.Model.prototype, 'save');
+            addMedal = medals.__get__("addMedal");
+        });
 
+        it("test addMedal on save success", async function () {
+            saveStub.resolves("");
+            await addMedal(req, res);
+            expect(res.status).to.equal(200);
+        });
+
+        it("test addMedal on save failure", async function () {
+            // invalid unit: l
+            saveStub.rejects("");
+            await addMedal(req, res);
+            expect(res.status).to.equal(500);
+        });
     });
 
-    it("test addMedal on save failure", async function () {
-        // invalid unit: l
-        req = { "body" :{"unit":"l", "amount": 10, "exerciseName":"Basketball", "level":1}};
-        sandbox.stub(mongoose.Model.prototype, 'save').rejects("");
-        let addMedal = medals.__get__("addMedal");
-        await addMedal(req, res);
-        expect(res.status).to.equal(500);
+    describe("Test getting medals", () => {
+        let medalsList;
+        let leanStub;
+        let sortStub;
+        let findStub;
 
-    });
+        beforeEach(() => {
+            req = {"session":{"username":"notexist"}};
+            medalsList = [{"exercise":{"exerciseName": "Basketball", "amount": 100, "unit":"min"}, "level":1}, {"exercise":{"exerciseName": "Basketball", "amount": 500, "unit":"min"}, "level":2}];
+            leanStub = sandbox.stub().resolves(medalsList);
+            sortStub = sandbox.stub().returns({lean:leanStub});
+            findStub = sandbox.stub(mongoose.Model, "find").returns({sort: sortStub});
+        });
+        it("test getInProgressMedals returns medals", async function () {
+            let getInProgressMedals = medals.__get__("getInProgressMedals");
+            findStub.returns({sort: sortStub});
+            await getInProgressMedals(req, res);
+            expect(res.status).to.equal(200);
+            expect(JSON.parse(res.data)).to.deep.equal(medalsList);
+        });
 
-    it("test getInProgressMedals returns medals", async function () {
-        let req = {"session":{"username":"notexist"}};
-        let medalsList = [{"exercise":{"exerciseName": "Basketball", "amount": 100, "unit":"min"}, "level":1}, {"exercise":{"exerciseName": "Basketball", "amount": 500, "unit":"min"}, "level":2}];
-        let getInProgressMedals = medals.__get__("getInProgressMedals");
-        let leanStub = sandbox.stub().resolves(medalsList)
-        let sortStub = sandbox.stub().returns({lean:leanStub})
-        sandbox.stub(mongoose.Model, "find").returns({sort: sortStub});
-        await getInProgressMedals(req, res);
-        expect(res.status).to.equal(200);
-        expect(JSON.parse(res.data)).to.deep.equal(medalsList);
-    });
-
-    it("test getCompletedMedals returns medals", async function () {
-        let req = {"session":{"username":"notexist"}}
-        let getCompletedMedals = medals.__get__("getEarnedMedals");
-        let medalsList = [{"exercise":{"exerciseName": "Basketball", "amount": 100, "unit":"min"}, "level":1}, {"exercise":{"exerciseName": "Basketball", "amount": 500, "unit":"min"}, "level":2}];
-        let leanStub = sandbox.stub().resolves(medalsList)
-        let sortStub = sandbox.stub().returns({lean:leanStub})
-        sandbox.stub(mongoose.Model, "find").returns({sort: sortStub});
-        await getCompletedMedals(req, res);
-        expect(res.status).to.equal(200);
-        expect(JSON.parse(res.data)).to.deep.equal(medalsList);
+        it("test getEarnedMedals returns medals", async function () {
+            findStub.returns({sort: sortStub});
+            let getEarnedMedals = medals.__get__("getEarnedMedals");
+            await getEarnedMedals(req, res);
+            expect(res.status).to.equal(200);
+            expect(JSON.parse(res.data)).to.deep.equal(medalsList);
+        });
     });
 });
